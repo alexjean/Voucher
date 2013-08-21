194c30a381239daa5e19ef7b8b0cb3377099eccd
alexseed <none@none>
2013-08-21 20:13:09 +0800

    Voucher2.26a 將[Ingredint]的IngredientID改成非自動編號,以免新年度複製食材時變成一號起編

--- VoucherExpense/Properties/AssemblyInfo.cs
+++ VoucherExpense/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      組建編號
 //      修訂編號
 //
-[assembly: AssemblyVersion("2.2.6.0")]
-[assembly: AssemblyFileVersion("2.2.6.0")]
+[assembly: AssemblyVersion("2.2.6.1")]
+[assembly: AssemblyFileVersion("2.2.6.1")]

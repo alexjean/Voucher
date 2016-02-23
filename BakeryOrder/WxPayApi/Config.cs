45b09f81bcc25c8a77b6390fb9085bcc456852d1
alexseed <none@none>
2016-02-23 18:18:18 +0800

    Bakery105 加入WxPayApi目錄 及 maidaren_cert憑証 為微信支付作準備

---
+++ BakeryOrder/WxPayApi/Config.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace WxPayAPI
+{
+    /**
+    * 	配置账号信息
+    */
+    public class WxPayConfig
+    {
+        //=======【基本信息设置】=====================================
+        /* 微信公众号信息配置
+        * APPID：绑定支付的APPID（必须配置）
+        * MCHID：商户号（必须配置）
+        * KEY：商户支付密钥，参考开户邮件设置（必须配置）
+        * APPSECRET：公众帐号secert（仅JSAPI支付的时候需要配置）
+        */
+        public const string APPID = "1231078001";   // 現代, 要代MCake協商
+        public const string MCHID = "1276614001";   // 麥達人商戶號
+        public const string KEY   = "1276614001";   // 支付密碼, 尚未設置
+        public const string APPSECRET = "51c56b886b5be869567dd389b3e5d1d6";   // 只有 JS才使用
+
+        //=======【证书路径设置】===================================== 
+        /* 证书路径,注意应该填写绝对路径（仅退款、撤销订单时需要）
+        */
+        public const string SSLCERT_PATH = "cert/apiclient_cert.p12";         // 在 HttpService.cs 己經被改成用 byte[] BakeryOrder.Properties.Resources.maidaren_cert 
+        public const string SSLCERT_PASSWORD = "1276614001";
+
+
+
+        //=======【支付结果通知url】===================================== 
+        /* 支付结果通知回调url，用于商户接收支付结果
+        */
+        public const string NOTIFY_URL = "http://paysdk.weixin.qq.com/example/ResultNotifyPage.aspx";
+
+        //=======【商户系统后台机器IP】===================================== 
+        /* 此参数可手动配置也可在程序中自动获取
+        */
+        public const string IP = "8.8.8.8";
+
+
+        //=======【代理服务器设置】===================================
+        /* 默认IP和端口号分别为0.0.0.0和0，此时不开启代理（如有需要才设置）
+        */
+        public const string PROXY_URL = "0.0.0.0:0";
+
+        //=======【上报信息配置】===================================
+        /* 测速上报等级，0.关闭上报; 1.仅错误时上报; 2.全量上报
+        */
+        public const int REPORT_LEVENL = 1;
+
+        //=======【日志级别】===================================
+        /* 日志等级，0.不输出日志；1.只输出错误信息; 2.输出错误和正常信息; 3.输出错误信息、正常信息和调试信息
+        */
+        public const int LOG_LEVENL = 0;
+    }
+}
